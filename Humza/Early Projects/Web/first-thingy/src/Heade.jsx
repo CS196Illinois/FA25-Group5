ca051734b773157682c4fb78511fb0c75548d829
hasif72 <yourpalhumza@gmail.com>
2025-10-02 20:22:27 -0500

    Humza

---
+++ Humza/Early Projects/Web/first-thingy/src/Heade.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return(
+        <Header>
+            <h1>My website</h1>
+            <nav>
+                <UL>
+                    <li><a href = "#">Home</a></li>
+                    <li><a href = "#">About</a></li>
+                    <li><a href = "#">Services</a></li>
+                </UL>
+            </nav>
+            <hr>
+            </hr>
+        </Header>
+    );
+}
+
+export default Header
