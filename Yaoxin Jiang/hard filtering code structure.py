5808b3b3391022073e397d367e9d3a52e9ff0f12
Kenny_JT <yaoxin.jiang@outlook.com>
2025-10-16 20:19:11 -0500

    new code (not finished)

--- Yaoxin Jiang/hard filtering code structure.py
+++ Yaoxin Jiang/hard filtering code structure.py
@@ -2,6 +2,8 @@
 #courses: [] -> integers representing the row of section
 #endlist: [[]] -> list of list for course sections
 import pandas as pd
+import numpy as np
+df = pd.read_csv(r'C:\Users\kenny\source\repos\FA25-Group5\Yaoxin Jiang\modified-2025-sp (with RMP).csv')
 
 
 def timeToInt(timeString):
@@ -9,57 +11,46 @@ def timeToInt(timeString):
     minutes = int(timeString.split(':')[1])
     amPm = timeString.split(' ')[1]
     timeInt = hours * 6 + minutes // 10
-    if amPm == 'pm':
+    if amPm == 'pm' & hours != 12: #12:00 pm is 12:00, not 24:00 and there is no 12:00 am in the data
         timeInt += 72
     return timeInt - 42 #7:00 is the start time
 
-def hardFilter_toTimespace(section): #unfinished, need to get data from dataset
-    timespace[5][90] = 0
-    if startTime == "ARRANGED":
+def hardFilter_toTimespace(sectionIndex):
+    timespace = np.zeros((5, 90), dtype=int) # 2D Int Array representing 5 days, 90 time slots (7:00-22:00, 10 min each)
+    if df.loc[sectionIndex, 'Start Time'] == "ARRANGED":
         return timespace
+
+    #if there is no weekday, the start time will be arranged, no need to consider this situation
     #return the timespace for the course in format of int[5][90]
-    for i in range(timeToInt(startTime), timeToInt(endTime)):
-        if week.contains('M'):
-            timespace[0][i] = 1
-        if week.contains('T'):
-            timespace[1][i] = 1
-        if week.contains('W'):
-            timespace[2][i] = 1
-        if week.contains('R'):
-            timespace[3][i] = 1
-        if week.contains('F'):
-            timespace[4][i] = 1
+    dayMap = {'M': 0, 'T': 1, 'W': 2, 'R': 3, 'F': 4}
+    for i in range(timeToInt(df.loc[sectionIndex, 'Start Time']), timeToInt(df.loc[sectionIndex, 'End Time'])):
+        for day in df.loc[sectionIndex, 'Days of Week']:
+            timespace[dayMap[day]][i] = 1
     return timespace
 
-def hardFilter_addTime(timespace1, timespace2): 
-    #add two timespace together
-    for i in range(5):
-        for j in range(90):
-            timespace1[i][j] = timespace1[i][j] + timespace2[i][j]
-    return timespace1
-
-def hardFilter_checkConflict(timespace):
-    #check conflict in the timespace
-    for i in range(5):
-        for j in range(90):
-            if timespace[i][j] > 1:
-                return True
-    return False
+#deleted hardFilter_addTime and hardFilter_checkConflict methods, merged them into hardFilter_dfs method
 
 #make sure the section group has no conflict
-def hardFilter_dfs(courses, timespace, sections, endlist):# unfinished need to make sure endlist works well
+def hardFilter_dfs(courses, timespace, sectionIndexes):# unfinished need to make sure endlist works well
     #return if all section has no conflict
 
-    df = pd.read_csv("modified-2025-sp")
+    
+    if len(courses) == 0:
+        return [sectionIndexes]
 
-    if courses.size == 0:
-        return [sections]
-
+    endlist = []
     # only using time data from 2025
-    for i in df[df['Subject'].str.contains(courses[0].split(' ')[0]) and df['Number'].str.contains(courses[0].split(' ')[1])]['Row']: #unfinished, check whether df is correct
-        newTimespace = hardFilter_addTime(timespace, hardFilter_toTimespace(i))
-        if not hardFilter_checkConflict(newTimespace):
-            endlist += hardFilter_dfs(courses[1:], newTimespace, sections + i)
+    #v1.0 has code: for i in df.index(df['Subject'] == courses[0].split(' ')[0] & df['Number'] == courses[0].split(' ')[1]):
+
+    #v1.1 for this part, I changed it to the following:
+    matchClass = ( (df['Subject'] == courses[0].split(' ')[0]) & (df['Number'] == courses[0].split(' ')[1]) )
+    if not df.loc[matchClass].empty: #check if there is any section for the course
+        for i in df.loc[matchClass].index:
+            newTimespace = timespace + hardFilter_toTimespace(i) #add the timespace of the new section to the existing timespace
+            if not np.any(newTimespace > 1):
+                endlist += hardFilter_dfs(courses[1:], newTimespace, sectionIndexes + [i])
+    else:
+        endlist += hardFilter_dfs(courses[1:], timespace, sectionIndexes) #if there is no section for the course, just skip it
 
     return endlist
 
@@ -69,7 +60,14 @@ def hardFilter_dfs(courses, timespace, sections, endlist):# unfinished need to m
 
 def hardFilter(inputTimespace, courses):
     #inputTimespace is the hard preference time constraint
-    return hardFilter_dfs(courses, inputTimespace, [], [])
-
+    return hardFilter_dfs(courses, inputTimespace, [])
 
 #need to consider NaN situations
+    #Actually we don't have to because for every course ther will definitely be a time range
+    #only need to consider when there is no section available
+        #done considering this situation
+
+#test code
+inputTimespace = np.zeros((5, 90), dtype=int)
+courses = ["MATH 241"]
+print(hardFilter(inputTimespace, courses))
