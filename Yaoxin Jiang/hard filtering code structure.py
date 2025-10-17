9c026a725d94ddd6d56b5a03ad3b2c05872cdf54
Kenny_JT <yaoxin.jiang@outlook.com>
2025-10-16 20:54:35 -0500

    hard filtering done Success!

--- Yaoxin Jiang/hard filtering code structure.py
+++ Yaoxin Jiang/hard filtering code structure.py
@@ -8,10 +8,10 @@ df = pd.read_csv(r'C:\Users\kenny\source\repos\FA25-Group5\Yaoxin Jiang\modified
 
 def timeToInt(timeString):
     hours = int(timeString.split(':')[0])
-    minutes = int(timeString.split(':')[1])
+    minutes = int(timeString.split(':')[1].split(' ')[0])
     amPm = timeString.split(' ')[1]
     timeInt = hours * 6 + minutes // 10
-    if amPm == 'pm' & hours != 12: #12:00 pm is 12:00, not 24:00 and there is no 12:00 am in the data
+    if (amPm == 'pm') & (hours != 12): #12:00 pm is 12:00, not 24:00 and there is no 12:00 am in the data
         timeInt += 72
     return timeInt - 42 #7:00 is the start time
 
@@ -43,7 +43,8 @@ def hardFilter_dfs(courses, timespace, sectionIndexes):# unfinished need to make
     #v1.0 has code: for i in df.index(df['Subject'] == courses[0].split(' ')[0] & df['Number'] == courses[0].split(' ')[1]):
 
     #v1.1 for this part, I changed it to the following:
-    matchClass = ( (df['Subject'] == courses[0].split(' ')[0]) & (df['Number'] == courses[0].split(' ')[1]) )
+    matchClass = ( (df['Subject'] == courses[0].split(' ')[0]) & (df['Number'] == int(courses[0].split(' ')[1])) )
+
     if not df.loc[matchClass].empty: #check if there is any section for the course
         for i in df.loc[matchClass].index:
             newTimespace = timespace + hardFilter_toTimespace(i) #add the timespace of the new section to the existing timespace
@@ -69,5 +70,5 @@ def hardFilter(inputTimespace, courses):
 
 #test code
 inputTimespace = np.zeros((5, 90), dtype=int)
-courses = ["MATH 241"]
+courses = ["CHEM 103", "MATH 241", "TE 200", "LAS 100", "LAS 101"]
 print(hardFilter(inputTimespace, courses))
